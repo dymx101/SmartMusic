e482c48fdd298a0dc2c5d790c318c4034136cff4
fx0883 <fx0883@qq.com>
2024-10-28 11:31:04 +0800

    Initial Commit

---
+++ SmartMusicTests/SmartMusicTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartMusicTests.swift
+//  SmartMusicTests
+//
+//  Created by 冯璇 on 2024/10/28.
+//
+
+import Testing
+@testable import SmartMusic
+
+struct SmartMusicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
